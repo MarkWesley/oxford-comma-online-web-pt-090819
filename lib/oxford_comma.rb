e4e39060c9c7615d5bb47bd9bbaf369beb81a35f
Mark Stone <markwstone85@gmail.com>
2019-09-17 03:55:49 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
+  array.join
+end
 
+def oxford_comma(array)
+  array.join(" and ")
+end
+
+def oxford_comma(array)
+  if array.size == 1 
+    array[0]
+  elsif array.size == 2 
+    array.join(" and ")
+  else
+    array[0..-2].join(', ') + ", and " + array[-1]
+  end
 end
